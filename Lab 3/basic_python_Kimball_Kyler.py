69b7cbb51d2de93b23e028a58d69e6dbf61501f9
KylerKimball <kyler.kimball02@gmail.com>
2024-01-29 17:28:58 -0700

    movie_webpage

---
+++ Lab 3/basic_python_Kimball_Kyler.py
@@ -0,0 +1,56 @@
+#this is a comment
+
+print("Hello World!")
+
+
+
+print("hello class")
+
+5+5
+
+print("Chewbacca") #this is an object type of string
+print(3.4) #this is a number. Technically a float
+
+
+a = 5
+b = 6
+print(a+b)
+
+
+#A = 6
+#a = 5
+#print(a)
+#print(A)
+
+student_gpa = 4.0
+print(type(student_gpa))
+
+user_name = "kkimball20"
+print("My name is " + user_name + " not Ingo")
+
+number = 145 * 17
+print(str(number) + " This is a string")
+
+
+def add_number(a,b):
+  output = a + b
+  return output
+ 
+print(add_number(4, 5))
+
+
+
+def say_hi(a):
+  print(a)
+
+say_hi("hi")
+
+
+name = "Bob"
+
+def say_hello():
+  name = "Matt"
+  print(name)
+
+say_hello()
+print(name)
